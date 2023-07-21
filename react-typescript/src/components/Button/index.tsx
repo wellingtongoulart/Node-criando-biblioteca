b25cf4819dbc1c02486427d7d0215ff9d85381b6
wellingtongoulart <wellingtongoulart@outlook.com>
2023-07-21 16:40:53 -0300

    feat: usando props para adicionar parametros a um componente.

--- react-typescript/src/components/Button/index.tsx
+++ react-typescript/src/components/Button/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import style from './Button.module.scss'
 
-class Botao extends React.Component {
+class Botao extends React.Component<{ texto: string }> {
   render() {
     return (
-      <button className={style.Botao}>
-        Button
+      <button className={style.botao}>
+        {this.props.texto}
       </button>
     )
   }
